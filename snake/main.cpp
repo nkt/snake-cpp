4c56c4ae0fd9001eb082a0390df9c73a1144adc9
Gusakov Nikita <dev@nkt.me>
2014-06-11 19:11:13 +0400

    Initial Commit

---
+++ snake/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  snake
+//
+//  Created by Gusakov Nikita on 11/06/14.
+//  Copyright (c) 2014 nkt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
